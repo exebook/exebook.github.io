81d58cb135b93b7b05265966902560fd55c340ca
yakov <exebook@gmail.com>
2015-05-15 18:53:20 +0300

    playsound

---
+++ playsound/data.js
@@ -0,0 +1,522 @@
+﻿var cn = ['女士们，','先生们，','同志们，','朋友们：',
+'新年钟声就要敲响,',
+'2013年即将来临.',
+'我很高兴',
+'通过中国国际广播电台,',
+'中央人民广播电台',
+'和中央电视台,',
+'向全国各族人民，',
+'向香港特别行政区同胞',
+'和澳门特别行政区同胞,',
+'向台湾同胞',
+'和海外侨胞,',
+'向世界各国和各地区的朋友们,',
+'致以新年的祝福！',
+'2012年,',
+'是中国改革开放',
+'和现代化建设',
+'取得显著成绩的一年。',
+'中国人民同心协力、',
+'攻坚克难，',
+'中国经济社会发展',
+'呈现稳中有进的良好态势，',
+'各项事业全面推进，',
+'人民生活持续改善。',
+'中国继续开展全方位外交，',
+'加强同各国的交流合作，',
+'积极参与解决国际和地区热点问题，',
+'为世界和平与发展',
+'作出了新的贡献。|',
+'2012年，',
+'是中国发展进程中',
+'具有重大而深远意义的一年。',
+'前不久召开的',
+'中国共产党',
+'第十八次全国代表大会',
+'和十八届一中全会，',
+'描绘了全面建成小康社会',
+'和深化改革开放的宏伟蓝图，',
+'顺利实现了中国共产党',
+'中央领导机构新老交替。',
+'当前，中国各族人民',
+'正紧密团结在',
+'以习近平同志为总书记的',
+'党中央周围，',
+'万众一心，锐意进取，',
+'为全面建成小康社会而奋斗。|',
+'2013年，',
+'中国政府和人民',
+'将高举中国特色社会主义伟大旗帜，',
+'坚持以科学发展为主题、',
+'以转变经济发展方式为主线，',
+'继续把握好',
+'稳中求进的',
+'工作总基调，',
+'全面推进',
+'改革开放',
+'和社会主义现代化建设，',
+'为实现党的十八大确定的目标',
+'任务开好局、',
+'起好步。',
+'我们将坚定不移贯彻',
+'“一国两制”、',
+'“港人治港”、',
+'“澳人治澳”、',
+'高度自治方针，',
+'深化交流合作，',
+'促进香港、',
+'澳门长期繁荣稳定。',
+'我们将继续推动',
+'两岸关系',
+'和平发展，',
+'造福两岸同胞，',
+'维护中华民族根本利益。|',
+'当前，国际形势继续发生',
+'深刻复杂变化。',
+'和平、发展、合作、',
+'共赢是各国人民的共同愿望，',
+'各国相互依存',
+'日益紧密。',
+'同时，国际金融危机',
+'影响深远，',
+'世界经济',
+'低速增长态势仍将延续，',
+'一些国家和地区动荡不安，',
+'世界仍然很不安宁。|',
+'人类只有一个地球，',
+'各国共处一个世界。',
+'世界和平与发展',
+'需要各国人民同舟共济、',
+'共同推进。',
+'中国人民始终是促进世界和平',
+'与发展的坚定力量。',
+'无论国际风云如何变幻，',
+'中国人民走和平发展道路的坚定决心',
+'决不会动摇。',
+'中国将一如既往',
+'在和平共处',
+'五项原则的基础上',
+'积极发展同各国的友好合作，',
+'积极推动',
+'采用和平方式',
+'妥善解决国际',
+'和地区热点问题，',
+'努力促进世界经济',
+'强劲、',
+'可持续、',
+'平衡增长，',
+'发挥负责任大国作用.|',
+'我相信，',
+'只要遵循各国人民意愿，',
+'顺应世界发展潮流，',
+'大家共同努力，',
+'就一定能把世界和平',
+'与发展的崇高事业',
+'不断推向前进，',
+'就一定能把各国人民福祉',
+'不断提高到新的水平。',
+'最后，',
+'我从北京',
+'祝大家',
+'在新的一年里',
+'幸福安康！',]
+
+eng= [
+'Ladies,','Gentlemen,','Comrades,','friends:',
+'New Year\'s bell is about to ring,',
+'2013 is coming.',
+'I am very happy',
+'By China Radio International',
+'The Central People\'s Broadcasting Station',
+'CCTV',
+'To people of all nationalities,',
+'In the Hong Kong compatriots',
+'And Macao special administrative regions,',
+'Taiwan compatriots,',
+'And overseas,',
+'To all countries and regions of the world friends,',
+'New Years greetings!',
+'2012',
+'China\'s reform and opening up',
+'And modernization',
+'Has made significant achievements of the year.',
+'Chinese people together,',
+'Tackle tough,',
+'China\'s economic and social development',
+'Showing a steady intake of good posture,',
+'All-round progress,',
+'People\'s lives continued to improve.',
+'China continues to carry out all-round diplomacy,',
+'Strengthen exchanges and cooperation with other countries,',
+'Active participation in the settlement of international and regional hot issues,',
+'To world peace and development',
+'To make a new contribution.|',
+'2012',
+'China\'s development process',
+'Has a significant and far-reaching significance of the year.',
+'Recently held',
+'Chinese Communist Party',
+'Eighteenth National People\'s Congress',
+'And the eighth in a plenary,',
+'Depicts the completion of a comprehensive well-off society',
+'And deepen the reform and opening up a grand blueprint',
+'Smooth realization of the Communist Party of China',
+'Old to the new central leading organs.',
+'At present, the Chinese people of all nationalities',
+'Working closely unite',
+'With Xi Jinping as general secretary',
+'Around the Central Committee,',
+'Unite as one, forge ahead,',
+'For building a moderately prosperous society in all respects.|',
+'2013',
+'The Chinese government and people',
+'China will hold high the great banner of socialism with characteristics',
+'Adhere to the scientific development as the theme,',
+'Transformation of economic development as the main line,',
+'Continue to hold good',
+'Maintaining stability',
+'Work overall tone,',
+'Promoting',
+'Reform and opening',
+'And socialist modernization',
+'To achieve the party\'s 18 goals set',
+'Task a good beginning,',
+'A good start.',
+'We will unswervingly implement the',
+'"One country",',
+'Hong Kong people ruling Hong Kong,',
+'Macao people governing Macao,',
+'High degree of autonomy',
+'Deepen exchanges and cooperation,',
+'Promote Hong Kong',
+'Long-term prosperity and stability of Macao.',
+'We will continue to drive',
+'Cross-strait relations',
+'Peaceful development',
+'Benefit compatriots on both sides,',
+'Safeguard the fundamental interests of the Chinese nation.|',
+'At present, the international situation continues to undergo',
+'Profound and complicated changes.',
+'Peace, development and cooperation,',
+'Win-win is the common wish of all peoples',
+'Interdependence',
+'Increasingly close.',
+'Meanwhile, the international financial crisis',
+'Far-reaching',
+'World economy',
+'Slow growth trend will continue,',
+'Some countries and regions in turmoil,',
+'The world is still far from tranquil.|',
+'We have only one earth,',
+'Countries in a world of coexistence.',
+'World peace and development',
+'Requires solidarity among peoples,',
+'Forge ahead.',
+'The Chinese people has always been to promote world peace',
+'Staunch force and development.',
+'No matter how the international situation changes,',
+'The Chinese people take the road of peaceful development firm determination',
+'Never waver.',
+'China will, as always',
+'Peaceful',
+'On the basis of the Five Principles',
+'Actively develop friendly cooperation with other countries,',
+'Actively promoting',
+'Adopt peaceful means',
+'Proper settlement of international',
+'And regional hot issues,',
+'Efforts to promote the world economy',
+'Strong',
+'Sustainable',
+'Balanced growth',
+'Plays the role of a responsible big country|.',
+'I believe,',
+'Just follow the wishes of the peoples',
+'Follow the world trend,',
+'We work together',
+'I will be able to world peace',
+'And the noble cause of development',
+'Continue to push forward,',
+'I will be able welfare of peoples',
+'Constantly raised to a new level.',
+'Finally,',
+'I\'m from Beijing',
+'I wish you',
+'In the new year',
+'Well-being!',]
+
+/***
+2012年,
+是中国改革开放
+和现代化建设
+取得显著成绩的一年。
+中国人民同心协力、
+攻坚克难，
+中国经济社会发展
+呈现稳中有进的良好态势，
+各项事业全面推进，
+人民生活持续改善。
+中国继续开展全方位外交，
+加强同各国的交流合作，
+积极参与解决国际和地区热点问题，
+为世界和平与发展
+作出了新的贡献。|
+2012年，
+是中国发展进程中
+具有重大而深远意义的一年。
+前不久召开的
+中国共产党
+第十八次全国代表大会
+和十八届一中全会，
+描绘了全面建成小康社会
+和深化改革开放的宏伟蓝图，
+顺利实现了中国共产党
+中央领导机构新老交替。
+当前，中国各族人民
+正紧密团结在
+以习近平同志为总书记的
+党中央周围，
+万众一心，锐意进取，
+为全面建成小康社会而奋斗。|
+2013年，
+中国政府和人民
+将高举中国特色社会主义伟大旗帜，
+坚持以科学发展为主题、
+以转变经济发展方式为主线，
+继续把握好
+稳中求进的
+工作总基调，
+全面推进
+改革开放
+和社会主义现代化建设，
+为实现党的十八大确定的目标
+任务开好局、
+起好步。
+我们将坚定不移贯彻
+“一国两制”、
+“港人治港”、
+“澳人治澳”、
+高度自治方针，
+深化交流合作，
+促进香港、
+澳门长期繁荣稳定。
+我们将继续推动
+两岸关系
+和平发展，
+造福两岸同胞，
+维护中华民族根本利益。|
+当前，国际形势继续发生
+深刻复杂变化。
+和平、发展、合作、
+共赢是各国人民的共同愿望，
+各国相互依存
+日益紧密。
+同时，国际金融危机
+影响深远，
+世界经济
+低速增长态势仍将延续，
+一些国家和地区动荡不安，
+世界仍然很不安宁。|
+人类只有一个地球，
+各国共处一个世界。
+世界和平与发展
+需要各国人民同舟共济、
+共同推进。
+中国人民始终是促进世界和平
+与发展的坚定力量。
+无论国际风云如何变幻，
+中国人民走和平发展道路的坚定决心
+决不会动摇。
+中国将一如既往
+在和平共处
+五项原则的基础上
+积极发展同各国的友好合作，
+积极推动
+采用和平方式
+妥善解决国际
+和地区热点问题，
+努力促进世界经济
+强劲、
+可持续、
+平衡增长，
+发挥负责任大国作用.|
+我相信，
+只要遵循各国人民意愿，
+顺应世界发展潮流，
+大家共同努力，
+就一定能把世界和平
+与发展的崇高事业
+不断推向前进，
+就一定能把各国人民福祉
+不断提高到新的水平。
+最后，
+我从北京
+祝大家
+在新的一年里
+幸福安康！
+***/
+var text = [
+'nǚshìmen,',
+'xiānshēngmen,',
+'tóngzhìmen,',
+'péngyǒumen:|',
+
+'xīnnián zhōng shēng jiù yào qiāo xiǎng,',
+'èrlíng-yīsān-nián jíjiāng láilín.',
+'Wǒ hěn gāoxìng',
+'tōngguò zhōngguó guójì guǎngbò diàntái,',
+'zhōngyāng rénmín guǎngbò diàntái',
+'hé zhōngyāng diànshìtái,',
+'xiàng quánguó gè zú rénmín,',
+'xiàng xiānggǎng tèbié xíngzhèngqū tóngbāo',
+'hé àomén tèbié xíngzhèngqū tóngbāo,',
+'xiàng táiwān tóngbāo',
+'hé hǎiwài qiáobāo,',
+'xiàng shìjiè gèguó hé gè dìqū de péngyǒumen,',
+'zhì yǐ xīnnián de zhùfú!|',
+
+
+'èrlíng-yīèr nián,',
+'shì zhōngguó gǎigé kāifàng',
+'hé xiàndàihuà jiànshè',
+'qǔdé xiǎnzhù chéngjī de yī nián.',
+'Zhōngguó rénmín tóngxīn xiélì,',
+'gōngjiān kè nán,',
+'zhōngguó jīngjì shèhuì fāzhǎn',
+'chéngxiàn wěn zhōng yǒu jìn de liánghǎo tàishì,',
+'gè xiàng shìyè quánmiàn tuījìn,',
+'rénmín shēnghuó chíxù gǎishàn.',
+'Zhōngguójìxù kāizhǎn quán fāngwèi wàijiāo,',
+'jiāqiáng tóng gèguó de jiāoliú hézuò,',
+'jījí cānyù jiějué guójì hé dìqū rèdiǎn wèntí,',
+'wèi shìjiè hépíng yǔ fāzhǎn',
+'zuòchūle xīn de gòngxiàn.|',
+
+'èrlíng-yīèr nián,',
+'shì zhōngguó fāzhǎn jìnchéng zhōng',
+'jùyǒu zhòngdà ér shēnyuǎn yìyì de yī nián.',
+'Qián bùjiǔ zhàokāi de',
+'zhōngguó gòngchǎndǎng',
+'dì shíbā cì quánguó dàibiǎo dàhuì',
+'hé shíbā jiè yī zhōng quánhuì,',
+'miáohuìle quánmiàn jiànchéng xiǎokāng shèhuì',
+'hé shēnhuà gǎigé kāifàng de hóngwěi lántú,',
+'shùnlì shíxiànle zhōngguó gòngchǎndǎng|',
+
+'zhōngyāng lǐngdǎo jīgòu xīn lǎo jiāotì.',
+'Dāngqián, zhōngguó gè zú rénmín',
+'zhèng jǐnmì tuánjié zài',
+'yǐ xíjìnpíng tóngzhì wèi zǒng shūjì de',
+'dǎng zhōngyāng zhōuwéi,',
+'wàn-zhòng-yī-xīn, ruìyì jìnqǔ,',
+'wèi quánmiàn jiànchéng xiǎokāng shèhuì ér fèndòu.|',
+
+'èr-líng-yī-sān-nián,',
+'zhōngguó zhèngfǔ hé rénmín',
+'jiāng gāojǔ zhōngguó tèsè shèhuì zhǔyì wěidà qízhì,',
+'jiānchí yǐ kēxué fāzhǎn wéi zhǔtí,',
+'yǐ zhuǎnbiàn jīngjì fāzhǎn fāngshì wéi zhǔxiàn,',
+'jìxù bǎwò hǎo',
+'wěn zhōng qiú jìn de',
+'gōng-zuò zǒng jīdiào,',
+'quánmiàn tuījìn',
+'gǎigé kāifàng',
+'hé shèhuì zhǔyì xiàndàihuà jiànshè,',
+'wèi shíxiàn dǎng de shíbā dà quèdìng de mùbiāo',
+'rènwù kāi hǎo jú,',
+'qǐ hǎo bù.',
+'Wǒmen jiāng jiāndìng bù yí guànchè',
+'“yīguóliǎngzhì”,',
+'“gǎng rénzhì gǎng”,',
+'“ào rénzhì ào”,',
+'gāodù zìzhì fāngzhēn,',
+'shēnhuà jiāoliú hézuò,',
+'cùjìn xiānggǎng,',
+'àomén chángqí fánróng wěndìng.',
+'Wǒmen jiāng jìxù tuīdòng',
+'liǎng\'àn guānxì',
+'hépíng fāzhǎn,',
+'zàofú liǎng\'àn tóngbāo,',
+'wéihù zhōnghuá mínzú gēnběn lìyì.|',
+
+
+'dāngqián, guójì xíngshì jìxù fāshēng',
+'shēnkè fùzá biànhuà.',
+'Hépíng, fāzhǎn, hézuò,',
+'gòng yíng shì gèguó rénmín de gòngtóng yuàn-wàng,',
+'gèguó xiāng-hù yīcún',
+'rìyì jǐnmì.',
+'Tóngshí, guójì jīnróng wéijī',
+'yǐng-xiǎng shēnyuǎn,',
+'shìjiè jīngjì',
+'dīsù zēng-zhǎng tàishì réng jiāng yánxù,',
+'yīxiē guójiā hé dìqū dòngdàng bù\'ān,',
+'shìjiè réng-rán hěn bù ānníng.|',
+
+
+'rénlèi zhǐyǒu yīgè dìqiú,',
+'gèguó gòngchǔ yīgè shìjiè.',
+'Shìjiè hépíng yǔ fāzhǎn',
+'xūyào gèguó rénmín tóng.zhōu.gòng.jì,',
+'gòngtóng tuījìn.',
+'Zhōngguó rénmín shǐzhōng shì cùjìn shìjiè hépíng',
+'yǔ fāzhǎn de jiāndìng lìliàng.',
+'Wúlùn guójì fēngyún rúhé biànhuàn,',
+'zhōngguó rénmín zǒu hépíng fāzhǎn dàolù de jiāndìng juéxīn',
+'jué bù huì dòngyáo.',
+'Zhōngguó jiāng yī-rú-jì-wǎng',
+'zài hépíng gòngchǔ',
+'wǔ xiàng yuánzé de jīchǔ shàng',
+'jījí fāzhǎn tóng gèguó de yǒuhǎo hézuò,',
+'jījí tuīdòng',
+'cǎiyòng hépíng fāngshì',
+'tuǒshàn jiějué guó jì',
+'hé dìqū rèdiǎn wèntí,',
+'nǔlì cùjìn shìjiè jīngjì',
+'qiáng-jìng,',
+'kě chíxù,',
+'pínghéng zēngzhǎng,',
+'fāhuī fù zérèn dàguó zuòyòng.|',
+
+'wǒ xiāngxìn,',
+'zhǐyào zūnxún gèguó rénmín yìyuàn,',
+'shùn-yìng shìjiè fāzhǎn cháoliú,',
+'dàjiā gòngtóng nǔlì,',
+'jiù yīdìng néng bǎ shìjiè hépíng',
+'yǔ fāzhǎn de chónggāo shìyè',
+'bùduàn tuī xiàng qián-jìn,',
+'jiù yīdìng néng bǎ gèguó rénmín fúzhǐ',
+'bùduàn tígāo dào xīn de shuǐpíng.|',
+
+'zuìhòu,',
+'wǒ cóng běijīng',
+'zhù dàjiā',
+'zài xīn de yī nián lǐ',
+'xìngfú ānkāng!|',
+
+]
+var at = [
+	15831,17492,19082,21149,
+	23041,26745,31110,33862,37205,40444,42987,
+		46563,50290,53285,56397,58498,63304,
+	69214,71188,74125,76412,79721,84841,87128,90762,95743,
+		99957,104172,109373,113692,119915,123885,
+	128738,
+	131084,134474,138862,142136,144609,148185,151041,156730,161400,
+	
+	167136,170573,175368,177818,180517,182508,
+	186908,191726,195534,198019,202674,207249,211533,214598,216501,
+	218429,221146,222829,225685,230434,233139,235124,239548,241475,
+	243785,245864,248151,251158,253584,257044,260794,262200,264010,266715,
+
+	271497,277457,279949,284512,288506,291547,293138,297100,
+	298815,300756,304365,308928,312945,317356,321234,325089,328943,
+	331010,336594,339578,343850,350000,351699,355333,357200,359652,
+	
+	365101,367511,369775,372200,374756,377860,379272,381060,382825,
+	386691,388909,392252,395654,398324,401970,404640,406974,410875,
+	413975,416413,418363,419529,421475,
+	424475,
+	
+]
+
